e94149439f1849280e56effa805b6c5bc99e3b72
Shriya Sorte <shriyasorte2000@gmail.com>
2024-06-24 15:02:14 +0530

    user contoller update

--- controller/userController.js
+++ controller/userController.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
+// const User = require('../model/userModel');
 const User = require('../model/userModel');
-const userModel = require('../model/userModel');
 
 async function adduser(req,res) {
     try {
         const user = new User(req.body);
         result = await user.save();
         res.status(201).send({message: "User added successfully", result});
-    } catch (error) {
+    } catch (err) {
         res.status(500).send(err);
     }
 }
@@ -15,16 +15,15 @@ async function adduser(req,res) {
 async function getuser(req,res) {
     try {
         const result = await User.find();
-       
         return res.status(200).send(result);
-    } catch (error) {
+    } catch (err) {
         res.status(500).send(err);
     }
 }
 
 async function updateuser(req,res){
     try {
-        const user = await userModel.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true});
         return res.status(200).send(user);
     } catch (err) {
         res.status(500).send(err);
@@ -33,7 +32,7 @@ async function updateuser(req,res){
 
 async function deleteuser(req,res){
     try {
-        const user = await userModel.findByIdAndDelete(req.params, req.body, {new:true});
+        const user = await User.findByIdAndDelete(req.params.id);
         return res.status(200).send(user);
     } catch (err) {
         res.status(500).send(err);
