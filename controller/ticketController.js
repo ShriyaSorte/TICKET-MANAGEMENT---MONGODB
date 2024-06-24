1795a5a4d7da3b88bd9c1c82b156f5767fd02325
Shriya Sorte <shriyasorte2000@gmail.com>
2024-06-24 15:01:56 +0530

    ticket controller code

--- controller/ticketController.js
+++ controller/ticketController.js
@@ -1,16 +1,43 @@
 const mongoose = require('mongoose');
+// const Ticket = require('../model/ticketModel');
 const Ticket = require('../model/ticketModel');
-const ticketModel = require('../model/ticketModel');
 
 async function addticket(req,res){
     try {
         const ticket = new Ticket(req.body);
         result = await ticket.save();
         res.status(201).send({message: "Ticket added successfully", result});
-    } catch (error) {
+    } catch (err) {
         res.status(500).send(err);
     }
 }
 
+async function getticket(req,res) {
+    try {
+        const result = await Ticket.find();
+        return res.status(200).send(result);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
 
-module.exports = {addticket};
+async function updateticket(req,res){
+    try {
+        const ticket = await Ticket.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        return res.status(200).send(ticket);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+async function deleteticket(req,res){
+    // console.log("ticket",req.params);   
+    try {
+        const result = await Ticket.findByIdAndDelete(req.params.id);
+        return res.status(200).send(result);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+module.exports = {addticket,getticket,updateticket,deleteticket};
